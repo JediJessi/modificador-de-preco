7fca6b1ff30d5c9aba92bc990229fe15120097dc
jessica080 <117183849+Jessica080@users.noreply.github.com>
2023-09-08 18:47:21 -0300

    apagando files nao utilizados

--- back/models/packProduct.js
+++
@@ -1,9 +0,0 @@
-// const Packs () => (
-//     constructor(id, pack_id, product_id, qty) {
-//       this.id = id
-//       this.pack_id = pack_id
-//       this.product_id = product_id
-//       this.qty = qty
-//     )
-
-// module.exports = Packs
